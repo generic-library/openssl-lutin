fff92985a97df1e20d70d746834f62d3a6ee5b41
Edouard DUPIN <yui.heero@gmail.com>
2016-05-05 21:25:55 +0200

    [DEV] compile crypto without asm optimisation

---
+++ generate/buildinf.h
@@ -0,0 +1,5 @@
+#ifndef MK1MF_BUILD
+    #define CFLAGS "compiler: Lutin build remove vompile Line"
+    #define PLATFORM "platform: Un-needed"
+    #define DATE "built on: Thu May  5 00:00:00 2555"
+#endif
